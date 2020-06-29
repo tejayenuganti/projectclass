bf42e9d5a3f3e30e2eb7fbdc30b39d92ab0d4163
Teja <tejasriyenuganti@gmail.com>
2020-06-29 09:40:38 +0530

    changes at local

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ public class first
 {
 public static void main (String[] args)
 {
-	System.out.println("first java proj updated secondtime in feature branch");
+	System.out.println("first java proj updated secondtime in feature branch , changes at local level");
 } 
 }
